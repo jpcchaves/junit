5e42562f5c50770acf9bb3da153332af880f99fa
Joao Chaves <=>
2024-07-27 10:06:05 -0300

    add subtraction test

--- src/test/java/com/junittests/SimpleMathTest.java
+++ src/test/java/com/junittests/SimpleMathTest.java
@@ -1,33 +1,60 @@
 package com.junittests;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 
 public class SimpleMathTest {
 
-    @Test
-    void testSum() {
-        SimpleMath sm = new SimpleMath();
+  @Test
+  void testSum() {
+    SimpleMath sm = new SimpleMath();
 
-        double firstNumber = 6.2D;
-        double secondNumber = 2D;
+    double firstNumber = 6.2D;
+    double secondNumber = 2D;
 
-        Double result = sm.sum(firstNumber, secondNumber);
+    Double result = sm.sum(firstNumber, secondNumber);
 
+    Double expected = 8.2D;
+    Double unexpected = 9.2D;
 
-        Double expected = 8.2D;
-        Double unexpected = 9.2D;
+    assertEquals(
+        expected,
+        result,
+        () ->
+            String.format(
+                "%s + %s did not produce %s!",
+                firstNumber, secondNumber, result));
 
-        assertEquals(
-                expected,
-                result,
-                () -> String.format("%s + %s did not produce %s!", firstNumber, secondNumber, result)
-        );
+    assertNotEquals(unexpected, result);
 
-        assertNotEquals(unexpected, result);
+    assertNotNull(result);
+  }
 
-        assertNotNull(result);
-    }
+  @Test
+  void testSubtraction() {
+
+    SimpleMath sm = new SimpleMath();
+
+    double firstNumber = 6.2D;
+
+    double secondNumber = 2D;
+
+    Double result = sm.subtraction(firstNumber, secondNumber);
+
+    Double expected = 4.2D;
+    Double unexpected = 9.2D;
+
+    assertNotNull(result);
+
+    assertEquals(
+        expected,
+        result,
+        () ->
+            String.format(
+                "%s - %s did dskjahfkhdaskfhsdakjfhkjasdhkjfhd not produce %s!",
+                firstNumber, secondNumber, result));
+
+    assertNotEquals(unexpected, result);
+  }
 }
