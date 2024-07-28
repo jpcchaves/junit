1c4ca079ee8f09f1c38b73744886ced073230b53
Joao Chaves <=>
2024-07-28 10:33:03 -0300

    add parameterized test

---
+++ src/test/java/com/junittests/SimpleMathTestS4.java
@@ -0,0 +1,55 @@
+package com.junittests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class SimpleMathTestS4 {
+  SimpleMath sm;
+
+  @BeforeEach
+  void beforeEachMethod() {
+    sm = new SimpleMath();
+  }
+
+  @DisplayName("Test division parameterized")
+  @ParameterizedTest
+  //  @MethodSource("testDivisionInputParameters")
+  @MethodSource
+  void testDivision(double firstNumber, double secondNumber, double expected) {
+
+    Double result = sm.division(firstNumber, secondNumber);
+
+    assertNotNull(result);
+
+    assertEquals(
+        expected,
+        result,
+        // decimals
+        2D,
+        () ->
+            String.format(
+                "%s / %s did not " + "produce %s",
+                firstNumber, secondNumber, expected));
+  }
+
+  public static Stream<Arguments> testDivision() {
+    return Stream.of(
+        Arguments.of(6.2D, 2D, 3.1D),
+        Arguments.of(71D, 14D, 5.07D),
+        Arguments.of(18.3D, 3.1D, 5.90D));
+  }
+
+  public static Stream<Arguments> testDivisionInputParameters() {
+    return Stream.of(
+        Arguments.of(6.2D, 2D, 3.1D),
+        Arguments.of(71D, 14D, 5.07D),
+        Arguments.of(18.3D, 3.1D, 5.90D));
+  }
+}
