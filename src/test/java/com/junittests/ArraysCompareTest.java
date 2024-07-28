76f9c58d7943b9535029027228b2c29d79577ec1
Joao Chaves <=>
2024-07-28 10:13:28 -0300

    add test timeout

--- src/test/java/com/junittests/ArraysCompareTest.java
+++ src/test/java/com/junittests/ArraysCompareTest.java
@@ -3,8 +3,11 @@ package com.junittests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class ArraysCompareTest {
 
@@ -20,5 +23,23 @@ public class ArraysCompareTest {
 
     // Then /Assert
     assertArrayEquals(numbers, expectedArr);
+  }
+
+  @DisplayName("Test performance when sorting array or throw timeout")
+  @Test
+  @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+  @Disabled(
+      "It was disabled to avoid slow execution, enable it to test the "
+          + "timeout exception")
+  void
+      testSortPerformance_When_SortingIt_ShouldHaveGoodPerformanceOrWillThrowTimeout() {
+    // Given / Arrange
+    int[] numbers = {25, 8, 21, 32, 3};
+
+    // When / Act
+    for (int i = 0; i < 1000000000; i++) {
+      numbers[0] = i;
+      Arrays.sort(numbers);
+    }
   }
 }
