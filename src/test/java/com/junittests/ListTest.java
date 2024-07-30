b664f8d269c28a5723c6964652ecad27d509fbc3
Joao Chaves <joao.chaves@liferay.com>
2024-07-30 11:26:12 -0300

    add list mock example

---
+++ src/test/java/com/junittests/ListTest.java
@@ -0,0 +1,60 @@
+package com.junittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ListTest {
+
+  @Test
+  void testMockingList_WhenSizeIsCalled_ShouldReturnTen() {
+
+    // Given
+    List<?> list = mock(List.class);
+
+    when(list.size()).thenReturn(10);
+
+    // When & Then
+    assertEquals(10, list.size());
+  }
+
+  @Test
+  void testMockingList_WhenSizeIsCalled_ShouldReturnMultipleValues() {
+
+    // Given
+    List<?> list = mock(List.class);
+
+    when(list.size()).thenReturn(10).thenReturn(20).thenReturn(20);
+
+    // When & Then
+    assertEquals(10, list.size());
+    assertEquals(20, list.size());
+    assertEquals(20, list.size());
+  }
+
+  @Test
+  void testMockingList_WhenGetIsCalled_ShouldReturnJoao() {
+
+    // Given
+    List list = mock(List.class);
+
+    when(list.get(0)).thenReturn("Joao");
+
+    // When & Then
+    assertEquals("Joao", list.get(0));
+  }
+
+  @Test
+  void testMockingList_WhenGetIsCalledWithArgumentMatcher_ShouldReturnJoao() {
+
+    // Given
+    List list = mock(List.class);
+
+    when(list.get(anyInt())).thenReturn("Joao");
+
+    // When & Then
+    assertEquals("Joao", list.get(anyInt()));
+  }
+}
