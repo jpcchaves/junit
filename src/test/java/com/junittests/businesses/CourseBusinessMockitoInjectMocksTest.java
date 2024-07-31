618d40ea7096866149de6282d4a264f84a568105
Joao Chaves <joao.chaves@liferay.com>
2024-07-31 08:12:04 -0300

    add most used mockito annotations

---
+++ src/test/java/com/junittests/businesses/CourseBusinessMockitoInjectMocksTest.java
@@ -0,0 +1,48 @@
+package com.junittests.businesses;
+
+import static org.mockito.Mockito.*;
+
+import com.junittests.business.CourseBusiness;
+import com.junittests.service.CourseService;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CourseBusinessMockitoInjectMocksTest {
+
+  @Mock CourseService mockService;
+
+  @InjectMocks CourseBusiness business;
+
+  @Captor ArgumentCaptor<String> argumentCaptor;
+
+  List<String> courses;
+
+  @BeforeEach
+  void setup() {
+
+    // Given / Arrange
+    business = new CourseBusiness(mockService);
+
+    courses =
+        Arrays.asList(
+            "REST API's RESTFul do 0 à Azure com ASP.NET Core 5 e Docker",
+            "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+            "Spotify Engineering Culture Desmistificado",
+            "REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker",
+            "Docker do Zero à Maestria - Contêinerização Desmistificada",
+            "Docker para Amazon AWS Implante Apps Java e .NET com Travis CI",
+            "Microsserviços do 0 com Spring Cloud, Spring Boot e Docker",
+            "Arquitetura de Microsserviços do 0 com ASP.NET, .NET 6 e C#",
+            "REST API's RESTFul do 0 à AWS com Spring Boot 3 Kotlin e Docker",
+            "Kotlin para DEV's Java: Aprenda a Linguagem Padrão do Android",
+            "Microsserviços do 0 com Spring Cloud, Kotlin e Docker");
+  }
+}
